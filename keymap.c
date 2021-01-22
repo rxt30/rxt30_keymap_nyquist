8010f219a929e27232f65102c6a0b0fd6a4323a3
rxt30 <mhampel@pm.me>
2021-01-22 08:48:58 +0100

    Better keymap for javascript

--- keymap.c
+++ keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, \
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_NUBS, KC_ENT, \
+  KC_GESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_NUBS, KC_ENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, DE_HASH, \
   KC_LCTL,  ADJUST, KC_LGUI, KC_ALGR, LOWER,   KC_BSPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | Reset|RGB Tg|RGB Md|Hue Up|Hue Dn|Sat Up|ü|ä|ö|      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  ä    |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|  PLAY    |      |
+ * |      |  ä    |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|  PLAY    |  `  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -176,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_TILD, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, _______, KC_LBRC, KC_QUOT, KC_SCLN , _______, KC_DEL, \
   _______, KC_QUOT, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLAY, _______, \
-  KC_BRID, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  KC_BRID, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, DE_GRV, \
   KC_BRIU, _______, _______, KC_LALT, _______, _______, _______, _______, KC_MPRV, KC_MUTE, KC_MPLY, KC_MNXT \
 )
 
